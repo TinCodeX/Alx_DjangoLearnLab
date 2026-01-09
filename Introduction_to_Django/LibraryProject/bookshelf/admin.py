1b2be5ef3e7369a1f978f07b4e5b7344b7ed164e
Tinsae Befekadu <tinsaebefekadu2012@gmail.com>
2026-01-09 23:02:42 +0300

    task 02

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Book
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publication_year')  # columns to display
+    search_fields = ('title', 'author')                     # enable search
+    list_filter = ('publication_year',)                     # filter by year
