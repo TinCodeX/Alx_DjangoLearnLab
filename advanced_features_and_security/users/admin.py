04a03df015364152f8eb66e45f988d8b24acd190
Tinsae Befekadu <tinsaebefekadu2012@gmail.com>
2026-01-22 23:59:38 +0300

    adding advanced_features_and_security

---
+++ advanced_features_and_security/users/admin.py
@@ -0,0 +1,29 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+
+class CustomUserAdmin(UserAdmin):
+    # Add your custom fields to admin
+    model = CustomUser
+    list_display = ('username', 'email', 'date_of_birth', 'is_staff', 'is_active')
+    list_filter = ('is_staff', 'is_active', 'date_of_birth')
+    
+    fieldsets = (
+        (None, {'fields': ('username', 'email', 'password')}),
+        ('Personal Info', {'fields': ('first_name', 'last_name', 'date_of_birth', 'profile_photo')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'date_of_birth', 'password1', 'password2', 'is_staff', 'is_active')}
+        ),
+    )
+    
+    search_fields = ('username', 'email')
+    ordering = ('username',)
+
+# Register the CustomUser with the admin
+admin.site.register(CustomUser, CustomUserAdmin)
---
+++ advanced_features_and_security/users/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+def user_list(request):
+    users = User.objects.all()
+    return render(request, 'users/user_list.html', {'users': users})
