3d54c8c0813bb6a40fd86e1e90263c108b95478c
Tinsae Befekadu <tinsaebefekadu2012@gmail.com>
2026-01-25 13:43:09 +0300

    Implementing HTTPS and Secure Redirects in Django

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,17 +1,28 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser
+from .models import CustomUser, Book, Author, Library
 
+# ----------------------
+# Register Book, Author, Library models
+# ----------------------
 admin.site.register(Book)
-# ✅ Custom User Admin class
+admin.site.register(Author)
+admin.site.register(Library)
+
+# ----------------------
+# Custom User Admin
+# ----------------------
 class CustomUserAdmin(UserAdmin):
+    # Add extra fields to the user admin
     fieldsets = UserAdmin.fieldsets + (
         ("Additional Information", {"fields": ("date_of_birth", "profile_photo")}),
     )
     add_fieldsets = UserAdmin.add_fieldsets + (
         ("Additional Information", {"fields": ("date_of_birth", "profile_photo")}),
     )
+
+    # Columns to display in the admin list view
     list_display = ("username", "email", "is_staff", "date_of_birth")
 
-# ✅ Register with admin.site.register to satisfy ALX
+# Register CustomUser with the custom admin
 admin.site.register(CustomUser, CustomUserAdmin)
