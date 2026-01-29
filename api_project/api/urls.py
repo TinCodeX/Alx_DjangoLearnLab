c655277381d2fa5904517e4130bfb3a521d8961e
Tinsae Befekadu <tinsaebefekadu2012@gmail.com>
2026-01-29 15:55:49 +0300

    Implement full CRUD for Book model using ViewSet and Router

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,8 +1,14 @@
-from django.urls import path
-from .views import BookList
+from django.urls import path, include
+from .views import BookList, BookViewSet
 from .views import home
+from rest_framework.routers import DefaultRouter
+
+# Create a router and register the ViewSet
+router = DefaultRouter()
+router.register(r'books_all', BookViewSet, basename='book_all')
 
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),
     path('', home, name='home'),
+    path('', include(router.urls)),
 ]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -5,9 +5,16 @@ from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
 from django.http import JsonResponse
+from rest_framework import viewsets
 
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 def home(request):
     return JsonResponse({"message": "Welcome to the Book API!"})
+class BookViewSet(viewsets.ModelViewSet):
+    """
+    A ViewSet for full CRUD operations on Book.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
